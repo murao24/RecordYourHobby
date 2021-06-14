b7f3dd218ae0ae0388ad4c357d05a5005891f565
Murao <yoshinobu612@icloud.com>
2021-06-14 09:51:16 +0900

    start coding

---
+++ Shared/RecordYourHobbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordYourHobbyApp.swift
+//  Shared
+//
+//  Created by 村尾慶伸 on 2021/06/14.
+//
+
+import SwiftUI
+
+@main
+struct RecordYourHobbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
